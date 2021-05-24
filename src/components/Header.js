bc169e80260693406cafe594bb6e412579f8b83f
DeletedC <CurtisWoodsArt@gmail.com>
2021-05-24 10:22:40 -0400

    Fixed mobile styling in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <div className='h-1/2 p-12 self-center text-center '>
-      <h1 className='text-7xl p-6'>Curtis J. Woods II</h1>
-      <h2>3D Artist turned Software Developer</h2>
+    <div className='flex flex-wrap h-1/2 p-12 self-center text-center justify-center'>
+      <h1 className='text-4xl sm:text-7xl p-6 whitespace-nowrap'>Curtis J. Woods II</h1>
+      <h2 className='w-4/5'>3D Artist turned Software Developer</h2>
     </div>
   );
 }
