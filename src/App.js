5101073382f2581416ba833a256671c1685ef596
DeletedC <CurtisWoodsArt@gmail.com>
2021-05-26 11:26:24 -0400

    Added Contact component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -30,7 +31,7 @@ function App() {
 
       {/* Contact */}
       <Section>
-
+        <Contact />
       </Section>
     </div>
 
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return (
+      <div>
+        <h1 className='text-center p-6'>Contact</h1>
+      </div>
+    );
+  }
+  
+  export default Contact;
