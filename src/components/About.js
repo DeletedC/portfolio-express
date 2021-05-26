a4c49a5d3d22a726a5a0c1d2ec76b3b3cb98df86
DeletedC <CurtisWoodsArt@gmail.com>
2021-05-26 11:13:35 -0400

    Fiddling with style in the about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,11 @@ const About = () => {
   return (
     <div>
       <h1 className='text-center p-6'>About</h1>
-      <div className='flex'>
-        <div className='px-6'>
-            <h2>Image placeholder!</h2>
-        </div>
-        <div className='px-6'>
-            <p>Hello! I'm Curtis. Welcome to my portfolio! I'm a full-stack software engineer with a background in art and I use that background to craft solid websites with a pleasant user experience.</p> 
-            <p>With a passion and hunger for improvement, I'm continuously learning new technologies and solidifying what I know through exploration, research, and ongoing projects. I use my skills to enable people to present themselves and their brand at their best to the world through the web.</p>
+      <div className='flex px-8 md:mx-12 justify-center rounded-xl bg-red-200'>
+        {/* <img src='/img/Headshot_20210524_Landscape.jpg' /> */}
+        <div className='py-6 md:p-10 text-xl tracking-wide md:leading-relaxed'>
+            <p className='pb-10'>Hello! I'm Curtis. Welcome to my portfolio! I'm a full-stack software engineer with a background in art and I use that background to craft solid websites with a pleasant user experience.</p> 
+            <p className='pb-10'>With a passion and hunger for improvement, I'm continuously learning new technologies and solidifying what I know through exploration, research, and ongoing projects. I use my skills to enable people to present themselves and their brand at their best to the world through the web.</p>
         </div>
       </div>
 
