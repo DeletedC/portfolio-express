0eeddd582845059015534903a87ebc70466c2882
DeletedC <CurtisWoodsArt@gmail.com>
2021-05-26 10:30:32 -0400

    Hero image set to cover

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className='w-full bg-fixed bg-top bg-contain bg-no-repeat bg-hero-portrait md:bg-hero-landscape'>
+    <div className='w-full bg-fixed bg-top bg-cover bg-no-repeat bg-hero-portrait md:bg-hero-landscape'>
       <div className='flex flex-wrap h-1/2 p-12 self-center text-center justify-center'>
         <h1 className='text-4xl sm:text-7xl whitespace-nowrap'>Curtis J. Woods II</h1>
         <h2 className='w-4/5'>3D Artist turned Software Developer</h2>
