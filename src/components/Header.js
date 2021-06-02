55d2154cda4af8eb38ffdb9dd297eee5002fbf24
DeletedC <CurtisWoodsArt@gmail.com>
2021-06-02 14:31:48 -0400

    Styled info box in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 const Header = () => {
   return (
     <div className='w-full bg-fixed bg-top bg-cover bg-no-repeat bg-hero-portrait md:bg-hero-landscape'>
-      <div className='bg-blue-100 bg-opacity-80 rounded-2xl md:w-1/2 md:h-1/2 flex flex-col my-24 mx-12 p-12 content-center justify-center'>
-        <h1 className='text-4xl sm:text-7xl whitespace-nowrap'>Curtis J. Woods II</h1>
-        <h2 className=''>3D Artist turned Software Developer</h2>
+
+      {/* Info Container */}
+      <div className='bg-blue-100 bg-opacity-80 rounded-2xl md:w-1/2 md:h-1/2 flex flex-col my-48 mx-12 p-12 content-center justify-center'>
+        
+        {/* Name and Title */}
+        <div>
+          <h1 className='text-4xl sm:text-7xl whitespace-nowrap'>Curtis J. Woods II</h1>
+          <h2 className=''>3D Artist turned Software Developer</h2>
+        </div>
+        
+
+        {/* Contact and Location Info */}
+        <div className='flex flex-col h-full justify-end text-xl'>
+          <p>Pittsburgh, PA</p>
+          <a href="mailto: CurtisJWoodsII@gmail.com">CurtisJWoodsII@gmail.com</a>
+          <a href="tel:724-777-8479">724-777-8479</a>
+        </div>
       </div>
     </div>
   );
