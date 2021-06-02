5a2e9ead3c2bcebc8fa7d84939e3d4a788483b10
DeletedC <CurtisWoodsArt@gmail.com>
2021-06-02 13:16:03 -0400

    Styled box for name, fixed header, added temp colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
     <div className='w-full bg-fixed bg-top bg-cover bg-no-repeat bg-hero-portrait md:bg-hero-landscape'>
-      <div className='flex flex-wrap h-1/2 p-12 self-center text-center justify-center'>
+      <div className='bg-blue-100 bg-opacity-80 rounded-2xl md:w-1/2 md:h-1/2 flex flex-col my-24 mx-12 p-12 content-center justify-center'>
         <h1 className='text-4xl sm:text-7xl whitespace-nowrap'>Curtis J. Woods II</h1>
-        <h2 className='w-4/5'>3D Artist turned Software Developer</h2>
+        <h2 className=''>3D Artist turned Software Developer</h2>
       </div>
     </div>
   );
