359b84b391399b4b2dd99340b09f4a08383c3b34
DeletedC <CurtisWoodsArt@gmail.com>
2021-06-03 09:43:17 -0400

    Fixed button for resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,22 @@ const Header = () => {
       <div className='md:bg-blue-100 md:bg-opacity-80 rounded-2xl md:w-1/2 md:h-1/2 flex flex-col md:my-48 md:mx-12 p-12 content-center justify-center'>
         
         {/* Name and Title */}
-        <div>
-          <h1 className='text-4xl sm:text-7xl whitespace-nowrap'>Curtis J. Woods II</h1>
+        <div className=''>
+          <h1 className='text-4xl sm:text-7xl whitespace-nowrap pb-4'>Curtis J. Woods II</h1>
           <h2 className=''>3D Artist turned Software Developer</h2>
         </div>
 
         {/* Contact and Location Info */}
-        <div className='flex flex-col h-full justify-end text-xl'>
+        <div className='relative flex flex-col h-full justify-end text-xl leading-relaxed'>
           <p>Pittsburgh, PA</p>
           <a href="mailto: CurtisJWoodsII@gmail.com">CurtisJWoodsII@gmail.com</a>
           <a href="tel:724-777-8479">724-777-8479</a>
-          <a href="./docs/Curtis Woods - Resume - 2021MayRevision.pdf" target="_blank">Resume</a>
+
+          {/* Button for Resume */}
+          <button className='absolute right-0 bg-green-600 rounded-lg p-5 px-20'
+            onClick={() => window.open("./docs/Curtis Woods - Resume - 2021MayRevision.pdf", "_blank")}
+          >Resumé</button>
+          
         </div>
       </div>
     </div>
