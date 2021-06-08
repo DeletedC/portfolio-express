14afd4a26c624193fc004991214c0b0093301ac8
DeletedC <CurtisWoodsArt@gmail.com>
2021-06-08 10:49:27 -0400

    Moved svg switch logic back to svgPicker.js

--- src/components/ProjectDiv.js
+++ src/components/ProjectDiv.js
@@ -1,14 +1,4 @@
-import DevIcon from "./DevIcon";
 import svgPicker from '../js/svgPicker'
-import { ReactComponent as JsSVG} from '../img/svg/javascript.svg';
-import { ReactComponent as ReactSVG} from '../img/svg/react.svg';
-import { ReactComponent as ExpressSVG} from '../img/svg/express.svg';
-import { ReactComponent as GitSVG} from '../img/svg/git.svg';
-import { ReactComponent as LinuxSVG} from '../img/svg/linux.svg';
-import { ReactComponent as MongoSVG} from '../img/svg/mongo.svg';
-import { ReactComponent as NodeSVG} from '../img/svg/node.svg';
-import { ReactComponent as PostgresSVG} from '../img/svg/postgres.svg';
-import { ReactComponent as PythonSVG} from '../img/svg/python.svg';
 
 
 const ProjectDiv = (props) => {
@@ -23,64 +13,7 @@ const ProjectDiv = (props) => {
             {/* DevIcons will go here */}
             <div className='flex flex-row flex-nowrap'>
             {props.tech.map((item) => {
-                switch (item) {
-                case "javascript":
-                    return (
-                    <DevIcon>
-                        <JsSVG />
-                    </DevIcon>
-                    );
-                case "react":
-                    return (
-                        <DevIcon>
-                            <ReactSVG />
-                        </DevIcon>
-                    );
-                case "node":
-                    return (
-                        <DevIcon>
-                            <NodeSVG />
-                        </DevIcon>
-                    );
-                case "express":
-                    return (
-                        <DevIcon>
-                            <ExpressSVG />
-                        </DevIcon>
-                    );
-                case "git":
-                    return (
-                        <DevIcon>
-                            <GitSVG />
-                        </DevIcon>
-                    );
-                case "linux":
-                    return (
-                        <DevIcon>
-                            <LinuxSVG />
-                        </DevIcon>
-                    );
-                case "mongoDB":
-                    return (
-                        <DevIcon>
-                            <MongoSVG />
-                        </DevIcon>
-                    );
-                case "python":
-                    return (
-                        <DevIcon>
-                            <PythonSVG />
-                        </DevIcon>
-                    );
-                case "postgres":
-                    return (
-                        <DevIcon>
-                            <PostgresSVG />
-                        </DevIcon>
-                    );
-                default:
-                    break;
-                }
+                return svgPicker(item);
             })} 
             </div>
 
--- src/js/svgPicker.js
+++ src/js/svgPicker.js
@@ -1,28 +1,73 @@
-import objSVG from './SVG';
+import DevIcon from '../components/DevIcon';
+import { ReactComponent as JsSVG} from '../img/svg/javascript.svg';
+import { ReactComponent as ReactSVG} from '../img/svg/react.svg';
+import { ReactComponent as ExpressSVG} from '../img/svg/express.svg';
+import { ReactComponent as GitSVG} from '../img/svg/git.svg';
+import { ReactComponent as LinuxSVG} from '../img/svg/linux.svg';
+import { ReactComponent as MongoSVG} from '../img/svg/mongo.svg';
+import { ReactComponent as NodeSVG} from '../img/svg/node.svg';
+import { ReactComponent as PostgresSVG} from '../img/svg/postgres.svg';
+import { ReactComponent as PythonSVG} from '../img/svg/python.svg';
 
 const svgPicker = (item) => {
     switch (item) {
         case "javascript":
-            return console.log (objSVG.javascript);
+            return (
+            <DevIcon>
+                <JsSVG />
+            </DevIcon>
+            );
         case "react":
-            return objSVG.react;
+            return (
+                <DevIcon>
+                    <ReactSVG />
+                </DevIcon>
+            );
         case "node":
-            return objSVG.node;
+            return (
+                <DevIcon>
+                    <NodeSVG />
+                </DevIcon>
+            );
         case "express":
-            return objSVG.express;
+            return (
+                <DevIcon>
+                    <ExpressSVG />
+                </DevIcon>
+            );
         case "git":
-            return objSVG.git;
+            return (
+                <DevIcon>
+                    <GitSVG />
+                </DevIcon>
+            );
         case "linux":
-            return objSVG.linux;
+            return (
+                <DevIcon>
+                    <LinuxSVG />
+                </DevIcon>
+            );
         case "mongoDB":
-            return objSVG.mongoDB;
+            return (
+                <DevIcon>
+                    <MongoSVG />
+                </DevIcon>
+            );
         case "python":
-            return objSVG.python;
+            return (
+                <DevIcon>
+                    <PythonSVG />
+                </DevIcon>
+            );
         case "postgres":
-            return objSVG.postgres;
+            return (
+                <DevIcon>
+                    <PostgresSVG />
+                </DevIcon>
+            );
         default:
             break;
-    }
+        }
 }
 
 export default svgPicker;
