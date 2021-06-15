c2f75904aae0103facc9152d796ac70a30a0c712
DeletedC <CurtisWoodsArt@gmail.com>
2021-06-15 12:06:41 -0400

    Updated style so text is at the start or end based on if the component is reversed

--- src/components/ProjectDiv.js
+++ src/components/ProjectDiv.js
@@ -5,23 +5,23 @@ const ProjectDiv = (props) => {
   return (
     
     // MAIN CONTAINER
-    <div className='flex flex-col items-center min-w-full px-12 py-12'>
-      <div className={`flex ${props.direction || 'flex-row'}`}>
+    <div className='flex flex-col items-center min-w-full py-12'>
+      <div className={`flex ${props.direction || 'flex-row'} px-12`}>
         
         {/* Project Name and Description */}
-        <div className='w-2/4'>
+        <div className={`flex flex-col w-2/4 ${props.direction? 'items-end': 'items-start'}`}>
             <h1 className='pb-4'>{props.name}</h1>
-            <p>Built With:</p>
+            <p className='text-xl pb-2'>Built With:</p>
 
             {/* DevIcons will go here */}
-            <div className='flex flex-row flex-nowrap'>
+            <div className={`flex flex-row flex-nowrap ${props.direction? 'justify-end': 'justify-start'}`}>
             {props.tech.map((item) => {
                 return svgPicker(item);
             })} 
             </div>
 
           {/* Project Description */}
-          <div className='text-2xl leading-relaxed'>
+          <div className={`flex flex-col ${props.direction? 'justify-end': 'justify-start'} text-2xl leading-relaxed py-12`}>
             {props.children}
           </div>
         </div>
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,6 @@
 const Section = (props) => {
   return (
-    <div className={`flex min-h-screen h-auto justify-center ${props.color}`}>
+    <div className={`flex min-h-screen justify-center ${props.color}`}>
       {props.children}
     </div>
   );
