f36f58830aa34b96730921b8006d6f88033818c1
DeletedC <CurtisWoodsArt@gmail.com>
2021-07-02 13:54:11 -0400

    Updated tailwind config to fix BG image, loads properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'hero-portrait': "url('/img/Headshot_Final_Portrait.jpg')",
+        'hero-landscape': "url('/img/Headshot_Final_Landscape.jpg')",
+      })
+    },
   },
   variants: {
     extend: {},
