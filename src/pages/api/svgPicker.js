0881323894d570ed059a0f34d4356391bc1b606d
DeletedC <CurtisWoodsArt@gmail.com>
2021-07-05 13:53:51 -0400

    Imported SVG.js from CRA build, fixed svgPicker to pull from there, dev icons now working on site again

--- src/pages/api/svgPicker.js
+++ src/pages/api/svgPicker.js
@@ -1,85 +1,74 @@
 import DevIcon from '../../components/DevIcon';
-// import { ReactComponent as JsSVG} from '../../img/svg/javascript.svg';
-// import { ReactComponent as ReactSVG} from '../../img/svg/react.svg';
-// import { ReactComponent as ExpressSVG} from '../../img/svg/express.svg';
-// import { ReactComponent as GitSVG} from '../../img/svg/git.svg';
-// import { ReactComponent as LinuxSVG} from '../../img/svg/linux.svg';
-// import { ReactComponent as MongoSVG} from '../../img/svg/mongo.svg';
-// import { ReactComponent as NodeSVG} from '../../img/svg/node.svg';
-// import { ReactComponent as PostgresSVG} from '../../img/svg/postgres.svg';
-// import { ReactComponent as PythonSVG} from '../../img/svg/python.svg';
-// import { ReactComponent as GmailSVG } from '../../img/svg/gmail_2020.svg';
-// import { ReactComponent as PhoneSVG } from '../../img/svg/phone.svg';
-
+import objSVG from './SVG';
 
 const svgPicker = (item) => {
     switch (item) {
         case "javascript":
             return (
             <DevIcon>
-                <img src='/img/svg/javascript.svg' />
+                {objSVG.javascript}
             </DevIcon>
             );
-        // case "react":
-        //     return (
-        //         <DevIcon>
-        //             <ReactSVG />
-        //         </DevIcon>
-        //     );
-        // case "node":
-        //     return (
-        //         <DevIcon>
-        //             <NodeSVG />
-        //         </DevIcon>
-        //     );
-        // case "express":
-        //     return (
-        //         <DevIcon>
-        //             <ExpressSVG />
-        //         </DevIcon>
-        //     );
-        // case "git":
-        //     return (
-        //         <DevIcon>
-        //             <GitSVG />
-        //         </DevIcon>
-        //     );
-        // case "linux":
-        //     return (
-        //         <DevIcon>
-        //             <LinuxSVG />
-        //         </DevIcon>
-        //     );
-        // case "mongoDB":
-        //     return (
-        //         <DevIcon>
-        //             <MongoSVG />
-        //         </DevIcon>
-        //     );
-        // case "python":
-        //     return (
-        //         <DevIcon>
-        //             <PythonSVG />
-        //         </DevIcon>
-        //     );
-        // case "postgres":
-        //     return (
-        //         <DevIcon>
-        //             <PostgresSVG />
-        //         </DevIcon>
-        //     );
-        // case "email":
-        //     return (
-        //         <DevIcon>
-        //             <GmailSVG />
-        //         </DevIcon>
-        //     );
-        // case "phone":
-        //     return (
-        //         <DevIcon>
-        //             <PhoneSVG />
-        //         </DevIcon>
-        //     )
+        case "react":
+            return (
+                <DevIcon>
+                    {objSVG.react}
+                </DevIcon>
+            );
+        case "node":
+            return (
+                <DevIcon>
+                    {objSVG.node}
+                </DevIcon>
+            );
+        case "express":
+            return (
+                <DevIcon>
+                    {objSVG.express}
+                </DevIcon>
+            );
+        case "git":
+            return (
+                <DevIcon>
+                    {objSVG.git}
+                </DevIcon>
+            );
+        case "linux":
+            return (
+                <DevIcon>
+                    {objSVG.linux}
+                </DevIcon>
+            );
+        case "mongoDB":
+            return (
+                <DevIcon>
+                    {objSVG.mongoDB}
+                </DevIcon>
+            );
+        case "python":
+            return (
+                <DevIcon>
+                    {objSVG.python}
+                </DevIcon>
+            );
+        case "postgres":
+            return (
+                <DevIcon>
+                    {objSVG.postgres}
+                </DevIcon>
+            );
+        case "email":
+            return (
+                <DevIcon>
+                    {objSVG.gmail}
+                </DevIcon>
+            );
+        case "phone":
+            return (
+                <DevIcon>
+                    {objSVG.phone}
+                </DevIcon>
+            )
         default:
             break;
         }
