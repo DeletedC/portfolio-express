2c8d415c57827769e44702cdbdd8d071e4cf2941
DeletedC <CurtisWoodsArt@gmail.com>
2021-08-10 23:22:58 -0400

    Updated project placeholders with actual descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,30 +9,27 @@ const Projects = () => {
       <h1 className='text-center pt-6'>Projects</h1>
       
       <div className=''>
-        <ProjectDiv name='Project Name Placeholder' 
-        tech={['react', 'mongoDB', 'heroku']} 
+        <ProjectDiv name='Piano Studio Student Tracker' 
+        tech={['mongoDB', 'express', 'react', 'node', 'heroku']} 
         image={'/img/projects/Screenshot_StudentTracker.png'}>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+          <p>I created this simple, yet specifically targeted app to be able to manage students.</p>
+          <p>Using JSX for the front end and MongoDB for the database, students can be added and removed from the roster, notes can be created, updated, and removed.</p> 
+          <p>Lastly, you can update or remove the lesson book that they’re currently working in.</p>
         </ProjectDiv>
-        <ProjectDiv name='Project Name Placeholder'
-        tech={['javascript', 'react']}
+        <ProjectDiv name='GetHub'
+        tech={['react', 'node']}
         image={"/img/projects/Screenshot_GetHub_Jon.jpeg"} 
         direction='flex-row-reverse'>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+          <p>DeveloperWeek Hackathon team project designed to browse and search multiple video services at the same time.</p> 
+          <p>I worked on the Amazon Prime portion and back end API to pull the data. The Amazon Prime portion was working, however it was cut for time.</p>
+          <p>Currently, the project uses OMDB as a test search API. I also removed a few bugs that would crash the app on android.</p>
         </ProjectDiv>
-        <ProjectDiv name='Project Name Placeholder' 
-        tech={['python', 'mongoDB']}
+        <ProjectDiv name='Intonation' 
+        tech={['mongoDB', 'express', 'react']}
         image={'/img/projects/Screenshot_Intonation.png'}>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-          <p>Description of the project, lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+          <p>A richer version of my student tracking app, using server-side rendering.</p>
+          <p>Using NextJs, it will include a roster, admin editing capabilities, teacher and student roles, and more thorough note and lesson tracking.</p>
+          <p>It currently has adding and deleting capabilities.</p>
         </ProjectDiv>
       </div>
     </div>
