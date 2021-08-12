d077390baf8250b68cf42ffb0c555a776932db8b
DeletedC <CurtisWoodsArt@gmail.com>
2021-08-12 13:13:45 -0400

    Fixed noreferrer issue in Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
 
         {/* CONTACT LINKS */}
         <div className='inline-flex md:pt-12'>
-          <a className='flex justify-center' href="https://www.linkedin.com/in/curtisjwoods" target='_blank'>{svgPicker('linkedIn')}</a>
+          <a className='flex justify-center' href="https://www.linkedin.com/in/curtisjwoods" target='_blank' rel='noreferrer'>{svgPicker('linkedIn')}</a>
           <a className='flex justify-center' href="mailto: CurtisJWoodsII@gmail.com">{svgPicker('email')}</a>
           <a className='flex justify-center' href="tel:724-777-8479">{svgPicker('phone')}</a>
         </div>
