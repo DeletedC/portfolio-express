f81fc5920a4f1b6388c05a5269135d3b72b17f7e
DeletedC <CurtisWoodsArt@gmail.com>
2021-08-12 12:49:31 -0400

    Added contact links in Header, updated svgPicker size parameters

--- src/components/DevIcon.js
+++ src/components/DevIcon.js
@@ -1,6 +1,6 @@
 const DevIcon = (props) => {
   return (
-    <div className={`w-1/4 md:w-1/12 ${props.padding}`}>
+    <div className={`w-1/4 ${props.extra || 'md:w-1/12'}`}>
       {props.children}
     </div>  
   );
--- src/pages/api/svgPicker.js
+++ src/pages/api/svgPicker.js
@@ -5,97 +5,97 @@ const svgPicker = (item, params) => {
     switch (item) {
         case "javascript":
             return (
-            <DevIcon key={item} padding={params}>
+            <DevIcon key={item} extra={params}>
                 {objSVG.javascript}
             </DevIcon>
             );
         case "react":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.react}
                 </DevIcon>
             );
         case "node":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.node}
                 </DevIcon>
             );
         case "express":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.express}
                 </DevIcon>
             );
         case "git":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.git}
                 </DevIcon>
             );
         case "linux":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.linux}
                 </DevIcon>
             );
         case "mongoDB":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.mongoDB}
                 </DevIcon>
             );
         case "python":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.python}
                 </DevIcon>
             );
         case "postgres":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.postgres}
                 </DevIcon>
             );
         case "email":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.gmail}
                 </DevIcon>
             );
         case "phone":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.phone}
                 </DevIcon>
             );
         case "heroku":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.heroku}
                 </DevIcon>
             );
         case "netlify":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.netlify}
                 </DevIcon>
             );
         case "html":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.html}
                 </DevIcon>
             );
         case "css":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.css}
                 </DevIcon>
             );
         case "linkedIn":
             return (
-                <DevIcon key={item} padding={params}>
+                <DevIcon key={item} extra={params}>
                     {objSVG.linkedIn}
                 </DevIcon>
             );
