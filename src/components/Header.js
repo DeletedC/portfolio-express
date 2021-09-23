19f710db7fb22e3066f69ac04e24bcc6ad67ab2b
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-09-23 00:26:54 -0400

    Decreased size of contact SVGs in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,14 +22,14 @@ const Header = () => {
 
           {/* Contact and Location Info */}
           <div className='relative flex flex-col h-full justify-end text-xl leading-relaxed'>
-            <p>Pittsburgh, PA</p>
-            <a href="mailto: CurtisJWoodsII@gmail.com">CurtisJWoodsII@gmail.com</a>
-            <div className='inline-flex md:pt-12'>
+            <p>Greater Pittsburgh Area, PA</p>
+            {/* <a href="mailto: CurtisJWoodsII@gmail.com">CurtisJWoodsII@gmail.com</a> */}
+            <div className='inline-flex md:pt-12 space-x-16'>
             <a className='flex justify-left transform hover:scale-110' 
               href="https://www.linkedin.com/in/curtisjwoods" 
-              target='_blank' rel="noreferrer">{svgPicker('linkedIn', ' ')}</a>
-            <a className='flex justify-left transform hover:scale-110' href="mailto: CurtisJWoodsII@gmail.com">{svgPicker('email', ' ')}</a>
-            <a className='flex justify-left transform hover:scale-110' href="tel:724-777-8479">{svgPicker('phone', ' ')}</a>
+              target='_blank' rel="noreferrer">{svgPicker('linkedIn', 'pr-10')}</a>
+            <a className='flex justify-left transform hover:scale-110' href="mailto: CurtisJWoodsII@gmail.com">{svgPicker('email', 'pr-10 pt-2')}</a>
+            <a className='flex justify-left transform hover:scale-110' href="tel:724-777-8479">{svgPicker('phone', 'pr-10')}</a>
             {/* Button for Resume */}
               <button className='bg-green-600 hover:bg-green-400 transform hover:scale-110 rounded-lg p-5 px-20'
                 onClick={() => window.open("./docs/Curtis Woods - GA Resume.pdf", "_blank", ['noopener', 'noreferrer'])}
@@ -51,12 +51,12 @@ const Header = () => {
 
       <div>
         {/* Blue background */}
-        <div className='md:hidden bg-blue-100 bg-opacity-80 flex flex-col p-12 content-center justify-center'>
+        <div className='md:hidden bg-blue-100 bg-opacity-80 flex flex-col p-4 content-center justify-center text-center'>
           
           {/* Name and Title */}
           <div className=''>
             <h1 className='text-4xl sm:text-7xl whitespace-nowrap pb-4'>Curtis J. Woods II</h1>
-            <h2 className=''>3D Artist turned Software Developer</h2>
+            <h2 className='text-xl'>3D Artist turned Software Developer</h2>
           </div>
         </div>
 
