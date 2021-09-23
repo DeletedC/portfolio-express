e2bb4d6e6fca8fdc298507411857947a581099c3
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-09-23 00:31:36 -0400

    Fixed sizing of mobile contact bar

--- src/components/MobileLinksBar.js
+++ src/components/MobileLinksBar.js
@@ -2,13 +2,13 @@ import svgPicker from "../pages/api/svgPicker";
 
 const MobileLinksBar = () => {
     return (
-        <div className="md:hidden bg-white h-20 fixed bottom-0 inset-x-0 border-t-2 border-gray-200">
-          <div className="flex justify-center pt-4">
-            <a className='flex justify-center' 
+        <div className="md:hidden bg-white h-18 fixed bottom-0 inset-x-0 border-t-2 border-gray-200">
+          <div className="flex justify-center py-4">
+            <a className='' 
               href="https://www.linkedin.com/in/curtisjwoods" 
-              target='_blank' rel="noreferrer">{svgPicker('linkedIn', ' ')}</a>
-            <a className='flex justify-center' href="mailto: CurtisJWoodsII@gmail.com">{svgPicker('email', 'pt-1')}</a>
-            <a className='flex justify-center' href="tel:724-777-8479">{svgPicker('phone', ' ')}</a>
+              target='_blank' rel="noreferrer">{svgPicker('linkedIn', 'px-4')}</a>
+            <a className='' href="mailto: CurtisJWoodsII@gmail.com">{svgPicker('email', 'px-4 pt-1')}</a>
+            <a className='' href="tel:724-777-8479">{svgPicker('phone', 'px-4')}</a>
             {/* Button for Resume */}
             <button className='bg-green-600 hover:bg-green-400 rounded-lg px-4 mx-4'
                 onClick={() => window.open("./docs/Curtis Woods - GA Resume.pdf", "_blank", ['noopener', 'noreferrer'])}
