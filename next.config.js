32b528d4a8945e079969e5981d80cb54835a800f
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-09-23 17:02:51 -0400

    Styled name to better change sizes, edited Header for readability

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return '20210923_502pm'
+  },
 }
