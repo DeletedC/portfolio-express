9b9bac9fad4a413be92fdd6504d097429e700557
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-10-06 12:07:44 -0400

    Changed build ID to use current UTC date and time

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
-    return 'd1519eb818c81ea3c50485f66ca75fc4f6ac2cc5'
+    return new Date().toUTCString();
   },
 }
